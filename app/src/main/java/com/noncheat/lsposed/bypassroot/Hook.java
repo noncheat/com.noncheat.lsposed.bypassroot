74805e77e13142f5218bd87e5e9d5cc51d9eb028
quyenvsp <quyenvsp@gmail.com>
2024-11-13 20:10:20 +0700

    feat: update logic bypass Accessibility

--- app/src/main/java/com/noncheat/lsposed/bypassroot/Hook.java
+++ app/src/main/java/com/noncheat/lsposed/bypassroot/Hook.java
@@ -1,7 +1,11 @@
 package com.noncheat.lsposed.bypassroot;
 
+import android.content.ContentResolver;
+import android.provider.Settings;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -14,11 +18,22 @@ public class Hook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
         Class<?> classAccessibilityManagerCompat = XposedHelpers.findClass("android.view.accessibility.AccessibilityManager", loadPackageParam.classLoader);
+        Class<?> classSecureSetting = XposedHelpers.findClass("android.provider.Settings.Secure", loadPackageParam.classLoader);
         XposedHelpers.findAndHookMethod(classAccessibilityManagerCompat, "getEnabledAccessibilityServiceList", int.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) {
                 List result = new ArrayList<>();
                 param.setResult(result);
+            }
+        });
+        XposedHelpers.findAndHookMethod(classSecureSetting, "getString", ContentResolver.class, String.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                String name = (String) param.args[1];
+                if (Objects.equals(name, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)) {
+                    String result = "";
+                    param.setResult(result);
+                }
             }
         });
     }
